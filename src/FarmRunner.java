d4121a9f04591d43da75b9cc4f7f9f11fc7f068b
Saad Ahmad <ahmadsaad58@gmail.com>
2018-09-11 00:29:28 -0500

    Update FarmRunner.java

--- src/FarmRunner.java
+++ src/FarmRunner.java
@@ -12,10 +12,8 @@ import javax.sound.sampled.*;
 	/*
  	*Runs the Farm, serves as tester for the program
  	*/
-public class FarmRunner
-{
-	public static void main(String args[]) throws IOException
-	{
+public class FarmRunner {
+	public static void main(String args[]) throws IOException {
 		Farm far  = new Farm();
 	}
 }
